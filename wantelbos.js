c4569c0378de3975ce1b764e2f9b15a7757cc237
Waldo Bastian <Waldo.Bastian@gmail.com>
2020-02-16 21:55:05 -0800

    Try loading JSON

--- wantelbos.js
+++ wantelbos.js
@@ -8,3 +8,23 @@ function selectSection(section) {
   if (x) x.style.display = "block";
   current_section = section;
 }
+
+function fetchJSON(url, cb) {
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var result = JSON.parse(this.responseText);
+      cb(result);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function onLoad(section) {
+  selectSection(section);
+  fetchJSON('graphs/recent_vix_data.json', function(vix_data) {
+    console.log(vix_data);
+  });
+}
