b70a3f959ab73e6c9fc3bc870565514a2a6dc827
Waldo Bastian <Waldo.Bastian@gmail.com>
2020-02-16 20:53:06 -0800

    Make it fancy

---
+++ wantelbos.js
@@ -0,0 +1,10 @@
+var current_section;
+
+function selectSection(section) {
+  var x;
+  x = document.getElementById(current_section);
+  if (x) x.style.display = "none";
+  x = document.getElementById(section);
+  if (x) x.style.display = "block";
+  current_section = section;
+}
